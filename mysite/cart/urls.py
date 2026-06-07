44b11d859002b181205f529c892cd977b1e3ce2d
Neo <mrrneo@outlook.com>
2026-06-07 08:20:38 +0600

    ...

--- mysite/cart/urls.py
+++ mysite/cart/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import *
+from .views import CartView, CartItemViewSet
 
 urlpatterns = [
-path('cart/', CartView.as_view()),
-path('cart/items/', CartItemViewSet.as_view({'get': 'list','post': 'create'})),
-path('cart/items/<int:pk>/', CartItemViewSet.as_view({'put': 'update','delete': 'destroy'})),
+    path('cart/', CartView.as_view()),
+    path('cart/items/', CartItemViewSet.as_view({'get': 'list', 'post': 'create'})),
+    path('cart/items/<int:pk>/', CartItemViewSet.as_view({'put': 'update', 'delete': 'destroy'})),
 ]
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,24 +1,8 @@
-from django.contrib import admin
-from django.urls import path,include
-from django.conf import settings
-from django.conf.urls.static import static
-from rest_framework_swagger.views import get_swagger_view
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-from rest_framework import permissions
-
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Store",
-        default_version='v1',),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
-
+from django.urls import path
+from .views import CartView, CartItemViewSet
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path( '', include('cart.urls')),
-    path('docs/', schema_view.with_ui('swagger', cache_timeout=0),name='schema-swagger-ui'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('cart/', CartView.as_view()),
+    path('cart/items/', CartItemViewSet.as_view({'get': 'list', 'post': 'create'})),
+    path('cart/items/<int:pk>/', CartItemViewSet.as_view({'put': 'update', 'delete': 'destroy'})),
+]
